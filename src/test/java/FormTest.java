9471bde7f6972eb56112dd61f93dcbf53485869c
PavelWhite64 <123714644+PavelWhite64@users.noreply.github.com>
2023-05-23 16:33:06 +0400

    Update FormTest.java

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -15,7 +15,6 @@ public class FormTest {
 
     @BeforeEach
     void setupTest() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
